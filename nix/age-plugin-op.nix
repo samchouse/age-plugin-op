34d68fa92baad069f879b2dfc0e9e096558e77e0
Samuel Corsi-House <sam@chouse.dev>
2024-11-02 18:38:41 -0400

    chore: bump version

--- nix/age-plugin-op.nix
+++ nix/age-plugin-op.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 pkgs.buildGoModule {
   pname = "age-plugin-op";
-  version = "0.2.0";
+  version = "0.2.1";
   src = ../.;
   vendorHash = "sha256-dhJdLYy/CDqZuF5/1v05/ZEp+cWJ6V4GnVCf+mUr1MU=";
   CGO_ENABLED = 0;
